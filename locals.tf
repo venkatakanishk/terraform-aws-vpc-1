e07133c846395eb128968b4ef95897b638ee1a7f
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-17 23:03:45 +0530

    changed something

---
+++ locals.tf
@@ -0,0 +1,14 @@
+locals {
+    common_tags = {
+        project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+    vpc_final_tags = merge(
+                            common_tags,
+                            {
+                            Name = "${var.project}-${var.environment}"
+                            },
+                            var.vpc_tags
+    )
+}
