b74b32974db64d79898c6279f196ff89efa2dd0c
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-17 23:45:20 +0530

    changed something

--- main.tf
+++ main.tf
@@ -7,7 +7,5 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = local.igw_final_tags
-  }
+  tags = local.igw_final_tags
 }
