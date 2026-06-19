933c875de4fa74d93177bc364cc9dda65f640cf4
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-19 23:03:19 +0530

    changed something

--- peering.tf
+++ peering.tf
@@ -16,25 +16,25 @@ resource "aws_vpc_peering_connection" "main" {
     allow_remote_vpc_dns_resolution = true
   }
 }
-resource "aws_route" "public" {
+resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection_main[count.index].id
 }
-resource "aws_route" "private" {
+resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection_main[count.index].id
 }
-resource "aws_route" "database" {
+resource "aws_route" "database_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection_main[count.index].id
 }
-resource "aws_route" "default" {
+resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id            = data.aws_route_table.default.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
